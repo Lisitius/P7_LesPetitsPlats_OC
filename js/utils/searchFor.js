9ccb05c2293a45b6859d9a3556e5babdd49005f4
unknown <benjamin.fichaux@gmail.com>
2023-02-16 17:19:27 +0100

    for

--- js/utils/searchFor.js
+++ js/utils/searchFor.js
@@ -1,46 +1,7 @@
-// // get search field HTML element
-// const inputSearch = document.querySelector("#search");
-
-// // Add an event listener to detect user input changes
-// inputSearch.addEventListener("input", () => {
-//   // Retrieve the value entered by the user and put it in lowercase
-//   const searchValue = inputSearch.value.toLowerCase();
-
-//   // Create an empty array to store recipes
-//   let filteredRecipes = [];
-
-//   // check the recipe table and add matching recipes to the search in the filtered table
-//   recipes.forEach((recipe) => {
-//     // Retrieve the recipe name, ingredients and description and put them in lowercase
-//     const name = recipe.name.toLowerCase();
-//     const ingredients = recipe.ingredients.map((ingredient) =>
-//       ingredient.ingredient.toLowerCase()
-//     );
-//     const description = recipe.description.toLowerCase();
-//     // Check if the name, ingredients or description contains the entered value
-//     if (
-//       name.includes(searchValue) ||
-//       ingredients.includes(searchValue) ||
-//       description.includes(searchValue)
-//     ) {
-//       // Add the check recipe in the filtered table
-//       filteredRecipes.push(recipe);
-//     }
-//   });
-
-//   // If the value entered is less than 3 characters, display all recipes
-//   if (searchValue.length < 3) {
-//     displayAllRecipe(recipes);
-//   } else {
-//     // Else, display the recipes corresponding to the search
-//     displayAllRecipe(filteredRecipes);
-//   }
-// });
-
 // Retrieve search field HTML element
 const inputSearch = document.querySelector("#search");
 
-// Retrieve full container HTML element
+// Retrieve full-container HTML element
 const fullContainer = document.querySelector(".full-container");
 
 // Retrieve error message HTML element
@@ -65,7 +26,8 @@ inputSearch.addEventListener("input", () => {
     // Create an array to store the filtered recipes
     const filteredRecipes = [];
     // Loop through each recipe in the recipes array
-    recipes.forEach((recipe) => {
+    for (let i = 0; i < recipes.length; i++) {
+      const recipe = recipes[i];
       const name = recipe.name.toLowerCase();
       const ingredients = recipe.ingredients.map((ingredient) =>
         ingredient.ingredient.toLowerCase()
@@ -80,7 +42,7 @@ inputSearch.addEventListener("input", () => {
         // If the recipe matches the search, add it to the filteredRecipes array
         filteredRecipes.push(recipe);
       }
-    });
+    }
     // Pass the filtered array to the displayAllRecipe function to display the results
     displayAllRecipe(filteredRecipes);
     // Display error message if no recipes match the search
