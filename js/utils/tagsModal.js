8891d6da50189ac3cc12712391c1bb62951bf408
unknown <benjamin.fichaux@gmail.com>
2023-02-23 04:58:01 +0100

    comments

--- js/utils/tagsModal.js
+++ js/utils/tagsModal.js
@@ -1,57 +1,75 @@
-//create arrays for modals
+// The "tagLists" function takes a list of recipes as an argument
 function tagLists(listRecipes) {
-  let ingredientsList = document.querySelector(".ingredient-list");
-  const ingredientsArray = [];
-  let devicesList = document.querySelector(".device-list");
-  const devicesArray = [];
-  let utensilsList = document.querySelector(".ustensil-list");
-  const utensilsArray = [];
+  // Retrieving lists of items from each category
+  const ingredientList = document.querySelector(".ingredient-list");
+  const deviceList = document.querySelector(".device-list");
+  const utensilList = document.querySelector(".ustensil-list");
 
-  //search elements and add in arrays
+  // Creation of tables to store tags for each category (ingredients, appliances and utensils)
+  const ingredientTags = [];
+  const deviceTags = [];
+  const utensilTags = [];
+
+  // Loop for each recipe in the recipe list
   listRecipes.forEach((recipe) => {
-    const ingredients = recipe.ingredients;
-    ingredients.forEach((ingredient) => {
-      let ingredientsTab = ingredient.ingredient;
-      console.log("Ingredient:", ingredientsTab);
-      ingredientsArray.push(
-        `<li class="ingredient_tag element" categorie="ingredient">${ingredientsTab.toLowerCase()}</li>`
-      );
+    // Loop for each recipe ingredient
+    recipe.ingredients.forEach((ingredient) => {
+      // Adding ingredients to the "ingredientTags" table if they are not already there
+      if (!ingredientTags.includes(ingredient.ingredient.toLowerCase())) {
+        ingredientTags.push(ingredient.ingredient.toLowerCase());
+      }
     });
 
-    const devices = recipe.appliance;
-    console.log("Device:", devices);
-    devicesArray.push(
-      `<li class="device_tag element" categorie="appliance">${devices.toLowerCase()}</li>`
-    );
+    // Adding devices to the "deviceTags" table if they are not already there
+    if (!deviceTags.includes(recipe.appliance.toLowerCase())) {
+      deviceTags.push(recipe.appliance.toLowerCase());
+    }
 
-    const utensils = recipe.ustensils;
-    utensils.forEach((ustensil) => {
-      console.log("Utensil:", ustensil);
-      utensilsArray.push(
-        `<li class="utensil_tag element" categorie="utensil">${ustensil.toLowerCase()}</li>`
-      );
+    // Loop for each recipe utensil
+    recipe.ustensils.forEach((ustensil) => {
+      // Adding utensils in the "utensilTags" table if they are not already there
+      if (!utensilTags.includes(ustensil.toLowerCase())) {
+        utensilTags.push(ustensil.toLowerCase());
+      }
     });
   });
-  //fin de search elements and add in arrays
 
-  //to display the arrays
-  let newIngredientsList = [...new Set(ingredientsArray)];
-  console.log("Ingredients List:", newIngredientsList);
-  ingredientsList.innerHTML += `<ul class="tag_list">${newIngredientsList.join(
-    ""
-  )}</ul>`;
-  let newDevicesList = [...new Set(devicesArray)];
-  console.log("Devices List:", newDevicesList);
-  devicesList.innerHTML += `<ul class="tag_list">${newDevicesList.join(
-    ""
-  )}</ul>`;
-  let newUtensilsList = [...new Set(utensilsArray)];
-  console.log("Utensils List:", newUtensilsList);
-  utensilsList.innerHTML += `<ul class="tag_list">${newUtensilsList.join(
-    ""
-  )}</ul>`;
-  //fin de to display the arrays
+  // HTML code for each list of tags
+  const ingredientListHTML =
+    '<ul class="tag-list">' +
+    ingredientTags
+      .map(
+        (ingredient) =>
+          `<li class="ingredient-tag element" categorie="ingredient">${ingredient}</li>`
+      )
+      .join("") +
+    "</ul>";
+
+  const deviceListHTML =
+    '<ul class="tag-list">' +
+    deviceTags
+      .map(
+        (device) =>
+          `<li class="device-tag element" categorie="appliance">${device}</li>`
+      )
+      .join("") +
+    "</ul>";
+
+  const utensilListHTML =
+    '<ul class="tag-list">' +
+    utensilTags
+      .map(
+        (utensil) =>
+          `<li class="utensil-tag element" categorie="utensil">${utensil}</li>`
+      )
+      .join("") +
+    "</ul>";
+
+  // Inserting the HTML code in the corresponding lists
+  ingredientList.innerHTML = ingredientListHTML;
+  deviceList.innerHTML = deviceListHTML;
+  utensilList.innerHTML = utensilListHTML;
 }
 
+// Call of the "tagLists" function with the list of recipes "recipes"
 tagLists(recipes);
-//fin de create arrays for modals
